2542d1f3e03183633c142bc4cc3337aefec4fb98
Alex Paklin <alexander.pak@prpl.global>
2021-11-28 22:26:31 +0500

    Deleting an Existing Book [HttpDelete]

--- my-books/Data/Services/BooksService.cs
+++ my-books/Data/Services/BooksService.cs
@@ -58,5 +58,15 @@ namespace my_books.Data.Services
 
             return _book;
         }
+
+        public void DeleteBookById(int bookId)
+        {
+            var _book = _context.Books.FirstOrDefault(book => book.Id == bookId);
+            if (_book != null)
+            {
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
+        }
     }
 }
