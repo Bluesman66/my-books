62a6775d4f29153b83171606ed50683681e3dc09
Alex Paklin <alexander.pak@prpl.global>
2021-12-01 14:59:21 +0500

    Many-to-Many Relationships in Entity Framework Core

--- my-books/Data/AppDbContext.cs
+++ my-books/Data/AppDbContext.cs
@@ -7,12 +7,28 @@ using System.Threading.Tasks;
 
 namespace my_books.Data
 {
-    public class AppDbContext: DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(ba => ba.Book)
+                .WithMany(b => b.Book_Authors)
+                .HasForeignKey(ba => ba.BookId);
+
+            modelBuilder.Entity<Book_Author>()
+                .HasOne(ba => ba.Author)
+                .WithMany(a => a.Book_Authors)
+                .HasForeignKey(ba => ba.AuthorId);
+        }
+
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book_Author> Books_Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
     }
 }
